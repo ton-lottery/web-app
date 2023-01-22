72f43bff8d53e615562ae753eb7a3eb9037a0c74
Konstantin <kostya.74.74@gmail.com>
2023-01-23 02:04:41 +0500

    background

--- src/App.js
+++ src/App.js
@@ -131,6 +131,24 @@ function App() {
     const [numbers, setNumbers] = useState([]);
     const [price, setPrice] = useState(0.2);
 
+    const stylesLight = {
+        background: {
+            height: '100%',
+            minHeight: '100vh',
+            backgroundColor: '#330033',
+            backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23404' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23505'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E")`
+        }
+    }
+
+    const stylesDark = {
+        background: {
+            height: '100%',
+            minHeight: '100vh',
+            backgroundColor: '#210021',
+            backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23404' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23505'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E")`
+        }
+    };
+
     const changeLanguage = (lng) => {
         setLocale(lng)
         localStorage.setItem('lng', lng)
@@ -276,8 +294,10 @@ function App() {
     }, [balance, getBet, getGamePlayed, getGuaranteedPrize, getOutAmount, getOwnerFee, isRun])
 
     return (
-        ready && <>
-            <Box sx={{flexGrow: 1}}>
+        ready && <div style={theme.palette.mode === 'dark' ? stylesDark.background : stylesLight.background}>
+            <Box
+                sx={{flexGrow: 1,}}
+            >
                 <AppBar position="static">
                     <Toolbar>
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
@@ -342,8 +362,9 @@ function App() {
                                                     value={(i + 1) + (5 * k)}
                                                     key={(i + 1) + (5 * k)}
                                                     disabled={numbers.length > 15 && !numbers.includes((i + 1) + (5 * k))}
-                                                    size="medium"
-                                                    fullWidth="true"
+                                                    size="small"
+                                                    fullWidth
+                                                    sx={{aspectRatio: '1 / 0.9'}}
                                                     onChange={(e, v) => {
                                                         if (numbers.includes((i + 1) + (5 * k)))
                                                             handleFormat(v * (-1));
@@ -371,14 +392,19 @@ function App() {
                                 {t("buy_button")} {price}💎
                             </Button>
                             <Tooltip title={t("clear_choice")}>
-                                <IconButton sx={{ml: 8}} disabled={numbers.length === 0} onClick={() => setNumbers([])}>
+                                <span>
+                                <IconButton sx={{ml: 16}} disabled={numbers.length === 0}
+                                            onClick={() => setNumbers([])}>
                                     <ClearIcon/>
                                 </IconButton>
+                                    </span>
                             </Tooltip>
                             <Tooltip title={t("random_choice")}>
+                                <span>
                                 <IconButton onClick={() => setNumbers(getRandom())}>
                                     <CasinoIcon/>
                                 </IconButton>
+                                    </span>
                             </Tooltip>
                         </Box>
                     </CardActions>
@@ -428,7 +454,7 @@ function App() {
                     </CardContent>}
                     </Card>
                 </Box>
-            </Container></>
+            </Container></div>
     );
 }
 
